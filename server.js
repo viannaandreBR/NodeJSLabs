32248de63aaaef27ee603a15344e61e77fb56919
GitLinuxDev <gitlinuxdev@gmail.com>
2019-08-31 16:10:24 -0300

    Ckeckpoint - Model Product.js

--- server.js
+++ server.js
@@ -1,19 +1,48 @@
 const  express = require('express');
 const mongoose = require("mongoose");
+const requireDir = require('require-dir');
 
 // Iniciando o App
 const app=express();
 
 // Iniciando o Data Base
 //mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true });
-mongoose.connect('mongodb://172.17.0.2:27017/nodeapi', {useNewUrlParser: true});
+
+mongoose.connect(
+    "mongodb://localhost:27017/nodeapi",
+     {useNewUrlParser: true}
+     );
+
+//mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true});
 
 //mongoose.connect('mongodb://localhost:27017/myapp', {useNewUrlParser: true});
 //var MyModel = mongoose.model('Test', new Schema({ name: String }));
 
+// sem a biblioteca require-dir
+require('./src/models/Product');
+
+
+// com a biblioteca require-dir
+requireDir('./src/models');
+
+const Product = mongoose.model("Product");
+
+
+
 // Criando a Primeira Rota
-app.get('/', (req,res) => {
-    res.send('NodeJS Labs...');
+app.get('/', ( req, res) => {
+    Product.create({
+        title: 'React Native',
+        description: 'Build native apps with React',
+        utl: 'http://github.com/facebook/react-native'
+    });                         
+
+    Product.create({
+        title: 'React JS',
+        description: 'Build FrontEnd Apps  with React',
+        utl: 'http://github.com/facebook/reactjs'
+    });                         
+    return res.send('NodeJS Labs...');
 });
 
 
