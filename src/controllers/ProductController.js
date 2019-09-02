075f09a4c838de909283070a73d081e93c08f622
GitLinuxDev <gitlinuxdev@gmail.com>
2019-09-01 23:18:19 -0300

    Update API Rest Método Show

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -2,13 +2,24 @@ const mongoose = require('mongoose');
 
 const Product = mongoose.model('Product');
 
+// CRUD
 module.exports = {
+  // Listagem
   async index(req, res) {
     const products = await Product.find();
 
     return res.json(products);
   },
+// Detalhe
 
+async show( req, res) {
+  const product = await Product.findById(req.params.id);
+
+  return res.json(product);
+},
+
+
+  // Criação
     async store(req,  res) {
       const product = await Product.create(req.body);
 
--- src/routes.js
+++ src/routes.js
@@ -4,8 +4,13 @@ const routes = express.Router();
 
 const ProductController = require('./controllers/ProductController');
 
+// Listagem
 routes.get("/products", ProductController.index);
 
+// Show
+routes.get("/products/:id",ProductController.show);
+
+// Criar Postagem
 routes.post('/products', ProductController.store);
 
 
